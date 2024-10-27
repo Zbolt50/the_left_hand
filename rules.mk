ce05482fb9aa12dfd3ffddb7707038135c5f6608
Zbolt50 <zekeboggs10@gmail.com>
2024-10-27 02:41:05 -0400

    Updated OLEDS and Joystick

--- rules.mk
+++ rules.mk
@@ -3,8 +3,14 @@ SERIAL_DRIVER = vendor
 
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes 
+MOUSEKEY_ENABLE = yes
 
+#Use if wanting Joystick as mouse
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = analog_joystick
+
+#Use if wanting joystick as arrows or WASD : WIP
+#JOYSTICK_ENABLE = yes
+#JOYSTICK_DRIVE = analog
 
 OLED_ENABLE = yes
